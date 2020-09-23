96db974d20816ef3132f7216fc5d1bdbe8c07959
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-09-23 20:38:33 +0200

    creating templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-@app.route('/JP2GMD')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+    
+@app.route('/beast')
+def beast():
+    return render_template('beast.html')
